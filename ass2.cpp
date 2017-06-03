7ffd4eb8545f28efaa16bc089431854a8c150f69
ElizabethSvit <eisvitanko@edu.hse.ru>
2017-06-03 17:01:22 +0300

    ass2.cpp updated
    An attempt to change the same file ass2.cpp according to comments in review.

--- ass2.cpp
+++ ass2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
@@ -9,7 +10,7 @@ using namespace std;
 // Task 1
 // 
 // Given a Binary Tree and a key, write a function that prints
-// all the ancestors of the key in the givenbinary tree.
+// all the ancestors of the key in the given binary tree.
 //
 
 struct node {
@@ -23,21 +24,43 @@ struct node {
     }
 };
 
-bool have_ancestors(struct node* root, int node_data) {
+bool have_ancestors(struct node* root, int node_data, vector<int> &nodes_to_print) {
     if (root == nullptr) {
         return false;
     }
-    // cout << root -> data << endl;
     
     if (root -> data == node_data) {
         return true;
     }
     
-    if (have_ancestors(root -> left_child, node_data) || have_ancestors(root -> right_child, node_data)) {
-        cout << root -> data << " ";
+    if (have_ancestors(root -> left_child, node_data, nodes_to_print) || have_ancestors(root -> right_child, node_data, nodes_to_print)) {
+        nodes_to_print.push_back(root -> data);
         return true;
     }
     return false;
+}
+
+void assert_output_task1(struct node* root, int node_data, string output, int case_num) {
+    std::stringstream ss;
+    auto old_buf = std::cout.rdbuf(ss.rdbuf());
+    
+    vector<int> nodes_to_print;
+    
+    have_ancestors(root, node_data, nodes_to_print);
+    for(int i = 0; i < nodes_to_print.size(); ++i) {
+        
+        cout << nodes_to_print[i] << " ";
+    }
+    
+    std::cout.rdbuf(old_buf); // reset
+    if (ss.str() == output) {
+        cout << "case " << case_num << " - OK" << endl;
+        return;
+    }
+    cout << "case " << case_num << " - FAIL for the reason: " << endl;
+    if (ss.str() != output) {
+        cout << "Function output: \"" << ss.str() << "\" is not equal to expected output: \"" << output  << "\"" << endl;
+    }
 }
 
 void test_task1() {
@@ -47,16 +70,7 @@ void test_task1() {
     root -> left_child = new node(9);
     root -> right_child = new node(18);
     
-    std::stringstream ss;
-    auto old_buf = std::cout.rdbuf(ss.rdbuf());
-    
-    have_ancestors(root, 18);
-    
-    std::cout.rdbuf(old_buf); // reset
-
-    if (ss.str() == "16 ") {
-        cout << "case 1 - OK" << endl;
-    }
+    assert_output_task1(root, 18, "16 ", 1);
     
     // case 2
     root = new node(16);
@@ -66,59 +80,22 @@ void test_task1() {
     root -> left_child -> right_child = new node(14);
     root -> left_child -> right_child -> right_child = new node(5);
     
-    old_buf = std::cout.rdbuf(ss.rdbuf());
-    ss.str(std::string());
-    
-    have_ancestors(root, 5);
-    
-    std::cout.rdbuf(old_buf); // reset
-    
-    if (ss.str() == "14 9 16 ") {
-        cout << "case 2 - OK" << endl;
-    }
+    assert_output_task1(root, 5, "14 9 16 ", 2);
     
     // case 3
     root = new node(16);
     
-    old_buf = std::cout.rdbuf(ss.rdbuf());
-    ss.str(std::string());
-    
-    have_ancestors(root, 16);
-    
-    std::cout.rdbuf(old_buf); // reset
-    
-    if (ss.str() == "") {
-        cout << "case 3 - OK" << endl;
-    }
+    assert_output_task1(root, 16, "", 3);
     
     // case 4
     root = nullptr;
     
-    old_buf = std::cout.rdbuf(ss.rdbuf());
-    ss.str(std::string());
-    
-    have_ancestors(root, 5);
-    
-    std::cout.rdbuf(old_buf); // reset
-    
-    if (ss.str() == "") {
-        cout << "case 4 - OK" << endl;
-    }
+    assert_output_task1(root, 5, "", 4);
     
     // case 5
     root = new node(16);
     
-    old_buf = std::cout.rdbuf(ss.rdbuf());
-    ss.str(std::string());
-    
-    have_ancestors(root, 5);
-    
-    std::cout.rdbuf(old_buf); // reset
-    
-    if (ss.str() == "") {
-        cout << "case 5 - OK" << endl;
-    }
-    
+    assert_output_task1(root, 5, "", 5);
 }
 
 // Task 2
@@ -127,23 +104,64 @@ void test_task1() {
 // of two nodes in a binary tree. Avoid storing additional nodes in a data structure.
 //
 
-struct node* find_low_com_ancestor(struct node* root, int n1, int n2) {
+struct node* find_low_com_ancestor(struct node* root, int n1, int n2, pair<bool, bool> &values_exists) {
     if (root == nullptr) {
         return nullptr;
     }
     
     if (root -> data == n1 || root -> data == n2) {
+        (root -> data == n1) ? values_exists.first = true : values_exists.second = true;
         return root;
     }
     
-    node* left_lca  = find_low_com_ancestor(root -> left_child, n1, n2);
-    node* right_lca = find_low_com_ancestor(root -> right_child, n1, n2);
+    node* left_lca  = find_low_com_ancestor(root -> left_child, n1, n2, values_exists);
+    node* right_lca = find_low_com_ancestor(root -> right_child, n1, n2, values_exists);
     
     if (left_lca && right_lca) {
         return root;
     }
+
+    return (left_lca != nullptr) ? left_lca : right_lca;
+}
+
+struct node* return_low_com_ancestor(struct node* root, int n1, int n2) {
+    pair<bool, bool> values_exists;
+    values_exists.first = false;
+    values_exists.second = false;
     
-    return (left_lca != NULL) ? left_lca : right_lca;
+    node* ancestor = find_low_com_ancestor(root, n1, n2, values_exists);
+    if (values_exists.first && values_exists.second) {
+        return ancestor;
+    } else {
+        return nullptr;
+    }
+}
+
+void assert_output_task2(node* tree_root, int n1, int n2, node* output_node, int case_num) {
+    
+    
+    node* ancestor = return_low_com_ancestor(tree_root, n1, n2);
+
+    if (ancestor == output_node) {
+        cout << "case " << case_num << " - OK" << endl;
+        return;
+    }
+    cout << "case " << case_num << " - FAIL for the reason: " << endl;
+    string func_output;
+    if (ancestor == nullptr) {
+        func_output = "nullptr";
+    } else {
+        func_output = to_string(ancestor -> data);
+    }
+    
+    string exp_output;
+    if (output_node == nullptr) {
+        exp_output = "nullptr";
+    } else {
+        exp_output = to_string(output_node -> data);
+    }
+    cout << "Function output: \"" << func_output << "\" is not equal to expected output: \"" << exp_output << "\"" << endl;
+    
 }
 
 void test_task2() {
@@ -153,9 +171,7 @@ void test_task2() {
     root -> left_child = new node(9);
     root -> right_child = new node(18);
     
-    if (find_low_com_ancestor(root, 9, 18) == root) {
-        cout << "case 1 - OK" << endl;
-    }
+    assert_output_task2(root, 9, 18, root, 1);
     
     // case 2
     root = new node(16);
@@ -165,23 +181,17 @@ void test_task2() {
     root -> left_child -> right_child = new node(14);
     root -> left_child -> right_child -> right_child = new node(5);
     
-    if (find_low_com_ancestor(root, 3, 14) == root -> left_child) {
-        cout << "case 2 - OK" << endl;
-    }
+    assert_output_task2(root, 3, 14, root -> left_child, 2);
     
     // case 3
     root = new node(16);
     
-    if (find_low_com_ancestor(root, 16, 14) == root) {
-        cout << "case 3 - OK" << endl;
-    }
+    assert_output_task2(root, 16, 14, nullptr, 3);
 
     // case 4
     root = nullptr;
     
-    if (find_low_com_ancestor(root, 16, 14) == nullptr) {
-        cout << "case 4 - OK" << endl;
-    }
+    assert_output_task2(root, 16, 14, nullptr, 4);
 
     // case 5
     root = new node(16);
@@ -192,9 +202,7 @@ void test_task2() {
     root -> right_child -> right_child = new node(10);
     root -> right_child -> right_child -> right_child = new node(50);
     
-    if (find_low_com_ancestor(root, 90, 50) == root) {
-        cout << "case 5 - OK" << endl;
-    }
+    assert_output_task2(root, 90, 50, root, 5);
     
 }
 

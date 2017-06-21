5cebfa6b932d430da2f304154e3584eb0aba233c
ElizabethSvit <eisvitanko@edu.hse.ru>
2017-06-22 00:03:26 +0300

    Updated assignment 3

--- ass3.cpp
+++ ass3.cpp
@@ -5,53 +5,52 @@
 
 using namespace std;
 
-#define ROW_SIZE 2
-#define COL_SIZE 3
-
-
 class Dictionary {
     set<string> dict_words;
     set<string> dict_prefixes;
 public:
     Dictionary() {};
     
-    void fill_dict(vector<string> words) {
-        for (int i = 0; i < words.size(); ++i) {
-            dict_words.insert(words[i]);
+    void fill_dict(const vector<string> &words) {
+        for (const string &word : words) {
+            dict_words.insert(word);
             
             string prefix = "";
-            for (int j = 0; j < words[i].length(); ++j) {
-                prefix += words[i][j];
+            for (char c : word) {
+                prefix +=c;
                 dict_prefixes.insert(prefix);
             }
         }
     }
 
 
-    bool isWord(string s) {
+    bool isWord(string const &s) {
         return dict_words.find(s) != dict_words.end();
     }
     
-    bool isPrefix(string s) {
+    bool isPrefix(string const &s) {
         return dict_prefixes.find(s) != dict_prefixes.end();
     }
 };
 
-void find_pref_word(char table[ROW_SIZE][COL_SIZE], vector<vector<bool> > visited, int i, int j, string &str, Dictionary dict, set<string> &result_words) {
-    visited[i][j] = true;
-    str += table[i][j];
+void find_pref_word(vector<vector<char>> &table, vector<vector<bool> > &visited, int y, int x, string &str, Dictionary dict, set<string> &result_words) {
+    int row_size = table.size();
+    int col_size = table[0].size();
+    
+    str += table[y][x];
     
     if (!dict.isPrefix(str)) {
         str.erase(str.length() - 1);
-        visited[i][j] = false;
         return;
     }
+    
+    visited[y][x] = true;
     
     if (dict.isWord(str))
         result_words.insert(str);
     
-    for (int row = i - 1; row <= i + 1 && row < ROW_SIZE; ++row) {
-        for (int col = j - 1; col <= j + 1 && col < COL_SIZE; ++col) {
+    for (int row = y - 1; row <= y + 1 && row < row_size; ++row) {
+        for (int col = x - 1; col <= x + 1 && col < col_size; ++col) {
             if (row >= 0 && col >= 0 && !visited[row][col]) {
                 find_pref_word(table, visited, row, col, str, dict, result_words);
             }
@@ -59,21 +58,17 @@ void find_pref_word(char table[ROW_SIZE][COL_SIZE], vector<vector<bool> > visite
     }
 
     str.erase(str.length() - 1);
-    visited[i][j] = false;
+    visited[y][x] = false;
 }
 
-void find_all_words(char table[ROW_SIZE][COL_SIZE], Dictionary dict, set<string> &result_words) {
-    vector<vector<bool> > visited;
-    vector<bool> row_visited(COL_SIZE, false);
-    for (int i = 0; i < ROW_SIZE; ++i) {
-        visited.push_back(row_visited);
-    }
+void find_all_words(vector<vector<char>> table, Dictionary const &dict, set<string> &result_words) {
+    vector<vector<bool> > visited(table.size(), vector<bool>(table[0].size(), false));
     
     string str = "";
 
-    for (int i = 0; i < ROW_SIZE; ++i) {
-        for (int j = 0; j < COL_SIZE; ++j) {
-            find_pref_word(table, visited, i, j, str, dict, result_words);
+    for (int y = 0; y < table.size(); ++y) {
+        for (int x = 0; x < table[y].size(); ++x) {
+            find_pref_word(table, visited, y, x, str, dict, result_words);
         }
     }
 }
@@ -82,13 +77,13 @@ int main(int argc, const char * argv[]) {
     Dictionary dict;
     dict.fill_dict({"CAR", "CARD", "CART", "CAT"});
 
-   char table[ROW_SIZE][COL_SIZE] = {{'A','A','R'}, {'T','C','D'}};
+    vector<vector<char>> table = {{'A','A','R'}, {'T','C','D'}};
 
     set<string> result_words;
     find_all_words(table, dict, result_words);
 
-    for (auto it = result_words.begin(); it != result_words.end(); ++it) {
-        std::cout << *it << endl;
+    for (const string &word : result_words) {
+        std::cout << word << endl;
     }
 
     return 0;

c0724bcce5a9d56fdbe0e69e9e988c5af1c5ab6d
ElizabethSvit <eisvitanko@edu.hse.ru>
2017-07-02 14:37:21 +0300

    Updated assignment 4 with second algoritm
    Left only recursive DFS algorithm from previous version and updated code with review comments being considered. Added the second Union-Find algorithm using Hint 2

--- ass4.cpp
+++ ass4.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <deque>
 
 using namespace std;
 
-// First variant of algorithm using recursion
+// Recursive "Flood fill" algorithm
 
-void flood_fill_recursive(vector<vector<bool> > &table, int y, int x) {
+void flood_fill_recursive(vector<vector<bool> > table, int y, int x) {
     if (y < 0 || y >= table.size() || x < 0 || x >= table[0].size()) {
         return;
     }
@@ -22,48 +21,111 @@ void flood_fill_recursive(vector<vector<bool> > &table, int y, int x) {
     flood_fill_recursive(table, y, x + 1);
 }
 
-// Second variant of algorithm using data structure deque for storing islands
-
-void flood_fill_queued(vector<vector<bool> > &table, int y, int x) {
-    deque<pair<int, int>> tile_deque;
-    tile_deque.push_back({y,x});
-    
-    while (!tile_deque.empty()) {
-        pair<int, int> cur_tile = tile_deque.front();
-    
-        tile_deque.pop_front();
-        
-        if (cur_tile.first - 1 >= 0 && table[cur_tile.first - 1][cur_tile.second]) {
-            table[cur_tile.first - 1][cur_tile.second] = false;
-            tile_deque.push_back({cur_tile.first - 1, cur_tile.second});
-        }
-        if (cur_tile.first + 1 < table.size() && table[cur_tile.first + 1][cur_tile.second]) {
-            table[cur_tile.first + 1][cur_tile.second] = false;
-            tile_deque.push_back({cur_tile.first + 1, cur_tile.second});
-        }
-        if (cur_tile.second - 1 >= 0 && table[cur_tile.first][cur_tile.second - 1]) {
-            table[cur_tile.first][cur_tile.second - 1] = false;
-            tile_deque.push_back({cur_tile.first, cur_tile.second - 1});
-        }
-        if (cur_tile.second + 1 < table[0].size() && table[cur_tile.first][cur_tile.second + 1]) {
-            table[cur_tile.first][cur_tile.second + 1] = false;
-            tile_deque.push_back({cur_tile.first, cur_tile.second + 1});
-        }
-    }
-}
-
-int count_islands(vector<vector<bool> > table) {
+int recursive_count_islands(vector<vector<bool> > table) {
     int counter = 0;
     for (int y = 0; y < table.size(); ++y) {
         for (int x = 0; x < table[y].size(); ++x) {
             if (table[y][x]) {
-                flood_fill_queued(table, y, x);
+                flood_fill_recursive(table, y, x);
                 counter++;
             }
         }
     }
     return counter;
 }
+
+// Algorithm "Union-Find" using Tree to store disjoint components as unique islands
+
+class Disjoint_sets {
+    vector<int> rank;
+    vector<int> parent;
+    
+public:
+    Disjoint_sets(int n) {
+        rank.resize(n, 0);
+        for (int i = 0; i < n; ++i) {
+            parent.push_back(i);
+        }
+    }
+    
+    int find(int x) {
+        if (parent[x] != x) {
+            return find(parent[x]);
+        }
+        return x;
+    }
+    
+    void func_union(int x, int y) {
+        int x_ancestor = find(x);
+        int y_ancestor = find(y);
+        
+        if (x_ancestor == y_ancestor) {
+            return;
+        }
+        
+        if (rank[x_ancestor] < rank[y_ancestor]) {
+            parent[x_ancestor] = y_ancestor;
+        }
+        
+        else if (rank[x_ancestor] > rank[y_ancestor]) {
+            parent[y_ancestor] = x_ancestor;
+        }
+        
+        else {
+            parent[x_ancestor] = y_ancestor;
+            rank[y_ancestor] = rank[y_ancestor] + 1;
+        }
+    }
+};
+
+int count_islands(vector<vector<bool> > table) {
+    int max_width = 0;
+    
+    for (int y = 0; y < table.size(); ++y) {
+        for (int x = 0; x < table[y].size(); ++x) {
+            if (max_width < table[y].size()) {
+                max_width = table[y].size();
+            }
+        }
+    }
+    
+    Disjoint_sets islands = Disjoint_sets(table.size() * max_width);
+    vector<pair<int, int>> delta_x_y = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    for (int y = 0; y < table.size(); ++y) {
+        for (int x = 0; x < table[y].size(); ++x) {
+            if (table[y][x]) {
+                for (auto delta : delta_x_y) {
+                    if (y + delta.first >= 0 && y + delta.first < table.size()
+                        && x + delta.second >= 0 && x + delta.second < table[y].size()
+                        && table[y + delta.first][x + delta.second]) {
+                        islands.func_union(y + delta.first * table[y + delta.first].size() + x + delta.second,
+                                           (y + delta.first) * table[y + delta.first].size() + x + delta.second);
+                    }
+                }
+            }
+        }
+    }
+
+    vector<int> components(table.size() * max_width);
+    
+    int counter = 0;
+    for (int y = 0; y < table.size(); ++y) {
+        for (int x = 0; x < table[y].size(); ++x) {
+            if (table[y][x]) {
+                int value = islands.find(y * table[y].size() + x);
+                
+                if (!components[value]) {
+                    counter++;
+                }
+                
+                components[value]++;
+            }
+        }
+    }
+    return counter;
+}
+
 
 int main(int argc, const char * argv[]) {
     vector<vector<bool> > table = {{0,0,1}, {1,0,0}, {1,1,0}};

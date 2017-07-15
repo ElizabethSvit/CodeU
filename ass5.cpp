28a467d227fd7d0d98b69beb1735b7da171d270d
ElizabethSvit <eisvitanko@edu.hse.ru>
2017-07-15 18:38:35 +0300

    Updated ass5
    Added a test case with one word and changed output of a function to a vector of chars to make assertion universal.

--- ass5.cpp
+++ ass5.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 class Graph {
     vector<vector<int> > adj_list;
+    void topologicalSortUtil(int v, vector<bool> &visited, stack<int> &stack);
     
 public:
     Graph () {
@@ -17,8 +18,6 @@ public:
     void addEdge(int v, int w) {
         adj_list[v].push_back(w);
     }
-    
-    void topologicalSortUtil(int v, vector<bool> &visited, stack<int> &stack);
     
     stack<int> topologicalSort(vector<int> letter_ids);
 };
@@ -48,39 +47,58 @@ stack<int> Graph::topologicalSort(vector<int> letter_ids) {
     return stack;
 }
 
-string return_alphabet(vector<string> words) {
+vector<char> return_alphabet(vector<string> words) {
     Graph graph;
-    vector<int> letter_ids;
+    vector<char> output_chars;
     
-    for (int i = 0; i < words.size() - 1; ++i) {
-        string str_first = words[i];
-        string str_sec = words[i + 1];
-        
-        for (int j = 0; j < min(str_first.length(), str_sec.length()); ++j) {
-            if (str_first[j] != str_sec[j]) {
-                graph.addEdge(str_first[j] - 'A', str_sec[j] - 'A');
-                letter_ids.push_back(str_first[j] - 'A');
-                letter_ids.push_back(str_sec[j] - 'A');
-                break;
-            }
+    vector<int> letter_ids;
+    stack<int> ordered_alphabet;
+    
+    if (words.size() == 1) {
+        for (char letter: words[0]) {
+            output_chars.push_back(letter);
         }
     }
     
-    stack<int> ordered_alphabet = graph.topologicalSort(letter_ids);
-    string output_str;
+    else {
+        for (int i = 0; i < words.size() - 1; ++i) {
+            string str_first = words[i];
+            string str_sec = words[i + 1];
+            
+            for (int j = 0; j < min(str_first.length(), str_sec.length()); ++j) {
+                if (str_first[j] != str_sec[j]) {
+                    graph.addEdge(str_first[j] - 'A', str_sec[j] - 'A');
+                    letter_ids.push_back(str_first[j] - 'A');
+                    letter_ids.push_back(str_sec[j] - 'A');
+                    break;
+                }
+            }
+        }
+
+        ordered_alphabet = graph.topologicalSort(letter_ids);
+    }
     
     while (!ordered_alphabet.empty()) {
-        output_str = output_str + (char) ('A' + ordered_alphabet.top()) + " ";
+        output_chars.push_back((char) ('A' + ordered_alphabet.top()));
         ordered_alphabet.pop();
     }
-    return output_str;
+    return output_chars;
 }
 
-void assert_output(string output_str, string exp_output, int case_num) {
+void assert_output(vector<char> output_str, vector<char> exp_output, int case_num) {
     if (output_str == exp_output) {
         cout << "Test case " << case_num << " - OK" << endl;
     } else {
-        cout << "Function output: \"" << output_str << "\" is not equal to expected output: \"" << exp_output << "\"" << endl;
+        
+        cout << "Function output chars: ";
+        for (char letter: output_str) {
+            cout << letter << " ";
+        }
+        cout << "is not equal to expected output chars: ";
+        for (char letter: exp_output) {
+            cout << letter << " ";
+        }
+        cout << endl;
         cout << "Please, check if the test input is lexicographically correct." << endl;
     }
 }
@@ -89,24 +107,31 @@ void test_func() {
     
     // case 1
     vector<string> words = {"maa", "aab", "ab", "c"};
-    string output_str = return_alphabet(words);
-    string exp_output = "m a c b ";
+    vector<char> output_str = return_alphabet(words);
+    vector<char> exp_output = {'m', 'a', 'c', 'b'};
     
     assert_output(output_str, exp_output, 1);
     
     // case 2
     words = {"A", "B", "c", "b"};
     output_str = return_alphabet(words);
-    exp_output = "A B c b ";
+    exp_output = {'A', 'B', 'c', 'b'};
     
     assert_output(output_str, exp_output, 2);
     
     // case 3
     words = {"C", "z", "Z", "c"};
     output_str = return_alphabet(words);
-    exp_output = "C z Z c ";
+    exp_output = {'C', 'z', 'Z', 'c'};
     
     assert_output(output_str, exp_output, 3);
+    
+    // case 4
+    words = {"ABC"};
+    output_str = return_alphabet(words);
+    exp_output = {'A', 'B', 'C'};
+    
+    assert_output(output_str, exp_output, 4);
 }
 
 
